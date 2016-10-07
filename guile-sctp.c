3de9e89950d14c96d63cdea34cc030d66a05becc
Michael Tuexen <tuexen@fh-muenster.de>
2016-10-07 15:27:22 +0200

    Cleanup.

--- guile-sctp.c
+++ guile-sctp.c
@@ -19,8 +19,12 @@
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
 #endif
+#ifdef HAVE_SYS_SOCKET_H
 #include <sys/socket.h>
+#endif
+#ifdef HAVE_NETINET_IN_H
 #include <netinet/in.h>
+#endif
 #ifdef HAVE_NETINET_SCTP_H
 #include <netinet/sctp.h>
 #endif
